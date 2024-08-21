ed29ccaefe767ab863eb29f1a3c700c3df4773b2
Deepak Singh <deepsinghkumar01@gmail.com>
2024-08-21 13:29:23 +0530

    Doctor entity class created...

---
+++ src/main/java/com/doctorapp/entity/Doctor.java
@@ -0,0 +1,43 @@
+package com.doctorapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "doctor")
+public class Doctor {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 45)
+    private String name;
+
+    @Column(name = "qualifiaction", nullable = false)
+    private String qualifiaction;
+
+    @Column(name = "about", length = 5000)
+    private String about;
+
+    @Column(name = "experience", nullable = false)
+    private String experience;
+
+    @Column(name = "image_url")
+    private String image_url;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false, unique = true, length = 1000)
+    private String password;
+
+    @Column(name = "mobile", length = 15)
+    private String mobile;
+
+}
