e0f5fcf3336694b79036313f4de1721cf58812d2
Deepak Singh <deepsinghkumar01@gmail.com>
2024-08-21 13:56:13 +0530

    Patient  and Doctor appointment entity created.

---
+++ src/main/java/com/doctorapp/entity/PatientDoctorAppointment.java
@@ -0,0 +1,35 @@
+package com.doctorapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "patient_doctor_appointment")
+public class PatientDoctorAppointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "appointment_date", nullable = false)
+    private LocalDate appointmentDate;
+
+    @Column(name = "appointment_time", nullable = false)
+    private String appointmentTime;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+}
