b2a9a6ec505a8b9243d92b281d70de350d57c62f
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-01 11:40:38 +0530

    Consulation time entity added.

---
+++ src/main/java/com/doctorapp/entity/Consultation_time.java
@@ -0,0 +1,30 @@
+package com.doctorapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "consultation_time")
+public class Consultation_time {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "local_date", nullable = false)
+    private LocalDate localDate;
+
+    @Column(name = "time", nullable = false)
+    private LocalTime time;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+}
