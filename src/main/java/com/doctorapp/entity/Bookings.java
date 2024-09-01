136771fbf2a27d0121c4fc84163fee5e01652c83
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-01 12:44:17 +0530

    Booking Entity added.

---
+++ src/main/java/com/doctorapp/entity/Bookings.java
@@ -0,0 +1,31 @@
+package com.doctorapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "bookings")
+public class Bookings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "date", nullable = false)
+    private String date;
+
+    @Column(name = "time", nullable = false)
+    private String time;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+}
