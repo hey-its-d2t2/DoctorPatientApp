27d5726e0f315eb30b94fbf881c54338707b6af9
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-01 12:25:15 +0530

    Doctor controller updated Consultation time end points are added.

--- src/main/java/com/doctorapp/Controller/DoctorController.java
+++ src/main/java/com/doctorapp/Controller/DoctorController.java
@@ -1,9 +1,12 @@
 package com.doctorapp.Controller;
 
+import com.doctorapp.entity.Consultation_time;
 import com.doctorapp.entity.Doctor;
+import com.doctorapp.repository.ConsultationTimeRepo;
 import com.doctorapp.repository.DoctorRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.print.Doc;
@@ -13,9 +16,11 @@ import java.util.List;
 @RequestMapping("/api/v1/doctor")
 public class DoctorController {
     private DoctorRepository doctorRepository;
+    private ConsultationTimeRepo consultationTimeRepo;
 
-    public DoctorController(DoctorRepository doctorRepository) {
+    public DoctorController(DoctorRepository doctorRepository, ConsultationTimeRepo consultationTimeRepo) {
         this.doctorRepository = doctorRepository;
+        this.consultationTimeRepo = consultationTimeRepo;
     }
 
    @GetMapping("/search")
@@ -41,4 +46,28 @@ public class DoctorController {
         List<Doctor> doctorsByArea = doctorRepository.findDoctorsBySpecializationAndCityOrArea("dentist", null, "BTM");
      }
     */
+
+//    consultation time
+ /*   @GetMapping("getConsultationInfo")
+    public  List<Consultation_time> getConsutationByDoctorId(long id){
+        return consultationTimeRepo.findByDoctorId(1L);
+    }*/
+
+
+//    ------
+    @GetMapping("/by-doctor")
+    public List<Consultation_time> getConsultationsByDoctorId(@RequestParam Long doctorId) {
+        return consultationTimeRepo.findByDoctorId(doctorId);
+    }
+    @GetMapping("/by-status")
+    public List<Consultation_time> getConsultationsByStatus(@RequestParam String status) {
+        return consultationTimeRepo.findByStatus(status);
+    }
+    @GetMapping("/by-status-and-doctor")
+    public List<Consultation_time> getConsultationsByStatusAndDoctorId(
+            @RequestParam String status,
+            @RequestParam Long doctorId) {
+        return consultationTimeRepo.findByStatusAndDoctorId(status, doctorId);
+    }
+
 }
